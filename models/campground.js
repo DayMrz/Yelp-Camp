94651a10e90d0c7d32198f00082e0a14ed2cdb9a
Leily <leilymrz@gmail.com>
2021-08-13 00:10:56 -0700

    deleting associate reviews for a given campground, mongoose middleware

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review')
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -15,6 +16,17 @@ const CampgroundSchema = new Schema({
     ]
 });
 
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    // console.log("Delete!")
+    // console.log(doc)
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema)
 
