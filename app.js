36f8dca73b13cdae76367d4d50c3a0724a8fbeb3
Leily <leilymrz@gmail.com>
2021-08-31 20:33:58 -0700

    creating path to use public directory, organizing files, checking validation forms

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ db.once('open', function () {
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 
@@ -37,18 +37,17 @@ app.get('/', (req, res) => {
     res.render('Home')
 });
 
-
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
 });
+
+
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh no, Something Went Wrong!'
     res.status(statusCode).render('error', { err });
 });
-
-
 
 app.listen(3000, () => {
     console.log('APP LISTENING ON PORT 3000')
