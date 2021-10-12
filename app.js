04644564493a4c78b7d4dfcbe2e971d82d323b08
Leily <leilymrz@gmail.com>
2021-10-12 11:25:59 -0700

    route for users created, registration form and post request have been added

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ const User = require('./models/user');
 
 const ExpressError = require('./Utilities/ExpressError');
 
-const campgrounds = require('./routes/campground');
-const reviews = require('./routes/review');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campground');
+const reviewRoutes = require('./routes/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -67,8 +68,9 @@ app.get('/fakeUser', async (req, res) => {
     res.send(newUser);
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
     res.render('Home')
--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
-    res.render('index', { user: req.user });
-})
+    res.render('users/register');
+});
 
+router.post('/register', async (req, res) => {
+    res.send(req.body);
+})
 
 
 module.exports = router;
