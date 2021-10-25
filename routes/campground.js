fc7a969a9b7c556993ead2123446497413b44972
Leily <leilymrz@gmail.com>
2021-10-25 15:22:27 -0700

    permissions for reviews, updating permission for delete button

--- routes/campground.js
+++ routes/campground.js
@@ -23,7 +23,12 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 
 router.get('/:id', catchAsync(async (req, res, next) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id).populate('reviews').populate('author');
+    const campground = await Campground.findById(id).populate({
+        path:'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
     console.log(campground);
     if (!campground) {
         req.flash('error', 'Cannot find that campground');
