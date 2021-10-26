31eae4a03bb7564cfa0169e289ee577fb16b22ac
Leily <leilymrz@gmail.com>
2021-10-25 21:02:29 -0700

    grouping routes  routes/users,js file

--- routes/users.js
+++ routes/users.js
@@ -4,25 +4,22 @@ const passport = require('passport');
 const catchAsync = require('../Utilities/catchAsync');
 const users = require('../controllers/user')
 
-router.get('/register', 
-    users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/register', 
-    catchAsync(users.register));
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate(
+            'local',
+            {failureFlash: true, 
+            failureRedirect: '/login' 
+            }
+        ), 
+        users.login
+    );
 
-router.get('/login', 
-    users.renderLogin);
 
-router.post(
-    '/login',
-    passport.authenticate(
-        'local',
-        { failureFlash: true, failureRedirect: '/login' }
-    ), 
-    users.login
-);
-
-router.get('/logout', 
-    users.logout)
+router.get('/logout', users.logout);
 
 module.exports = router;
