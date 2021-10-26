c3850d7261e3205d7b9829b03cb1979fe98be17f
Leily <leilymrz@gmail.com>
2021-10-26 14:30:20 -0700

    allowing multiple image files to upload

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const passport = require('passport');
 const localStrategy = require('passport-local');
 const User = require('./models/user');
 
+
 const ExpressError = require('./Utilities/ExpressError');
 
 const userRoutes = require('./routes/users');
--- routes/campground.js
+++ routes/campground.js
@@ -3,13 +3,19 @@ const router = express.Router()
 const catchAsync = require('../Utilities/catchAsync');
 const campgrounds = require('../controllers/campground');
 const {isLoggedIn, validateCampground, isAuthor} = require('../middleware');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post( 
-        isLoggedIn, 
-        validateCampground, 
-        catchAsync(campgrounds.createCampground));
+    // .post( 
+    //     isLoggedIn, 
+    //     validateCampground, 
+    //     catchAsync(campgrounds.createCampground));
+    .post(upload.single('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send('Yay :3');
+    })
 
 router.get('/new', 
     isLoggedIn, 
