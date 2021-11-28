3df1dabf8b09c991705f65f33840c34fba1a2254
Leily <leilymrz@gmail.com>
2021-11-27 16:39:20 -0800

    organizing app.js file

--- app.js
+++ app.js
@@ -3,29 +3,25 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require('express');
-const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const methodOverride = require('method-override');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
+const ExpressError = require('./Utilities/ExpressError');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const localStrategy = require('passport-local');
 const User = require('./models/user');
-const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
-
-
-const ExpressError = require('./Utilities/ExpressError');
-
+const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campground');
 const reviewRoutes = require('./routes/review');
+
+const MongoStore = require('connect-mongo');
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
-// 'mongodb://localhost:27017/yelp-camp'
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -39,15 +35,46 @@ db.once('open', function () {
     console.log('we are connected!');
 });
 
+const app = express();
+
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize({replaceWith: '_',}));
-app.use(helmet());
 
+const secret = process.env.SECRET || 'isnotasecret';
+
+const store = MongoStore.create({ 
+    mongoUrl: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function(e) {
+    console.log("SESSION STORE ERROR", e)
+});
+
+const sessionConfig = {
+    store,
+    name: 'session',
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        HttpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig));
+app.use(flash())
+app.use(helmet());
 
 const scriptSrcUrls = [
     "https://cdn.jsdelivr.net",
@@ -92,33 +119,6 @@ app.use(
         },
     })
 );
-const secret = process.env.SECRET || 'isnotasecret';
-
-const store = MongoStore.create({ 
-    mongoUrl: dbUrl,
-    secret,
-    touchAfter: 24 * 60 * 60
-})
-store.on("error", function(e) {
-    console.log("SESSION STORE ERROR", e)
-})
-
-const sessionConfig = {
-    store,
-    name: 'session',
-    secret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        HttpOnly: true,
-        // secure: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
-
-app.use(session(sessionConfig));
-app.use(flash())
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -129,12 +129,11 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    // console.log(req.session);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
-})
+});
 
 app.use('/', userRoutes);
 app.use('/campgrounds', campgroundRoutes);
@@ -147,8 +146,6 @@ app.get('/', (req, res) => {
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
 });
-
-
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
